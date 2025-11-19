2256df6d861f8a5d443d5874a92f89f28f917855
Mehedi Hassan Piash <piash599@gmail.com>
2025-11-19 20:29:56 +0600

    Improve logging message

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USER || process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '123',
+  password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'ecommerce_db',
   synchronize: false,
   logging: false,
